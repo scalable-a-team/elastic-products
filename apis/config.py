e60054a0f79a4a2adab9c8c9ad0f01b7078c32fa
Justyn Lantz <68471166+jtlantz@users.noreply.github.com>
2022-07-12 19:18:40 +0700

    add usability for es with aws

--- apis/config.py
+++ apis/config.py
@@ -1,3 +1,5 @@
-ELASTIC_PREFIX = 'search' #IMPORTANT: This will be static prefix for frontend to querey product search engine, can just change this to change all apis
+ELASTIC_PREFIX = 'es' #IMPORTANT: This will be static prefix for frontend to querey product search engine, can just change this to change all apis
 
-ELASTIC_URI = 'http://localhost:9200'
+ES_HOST = 'https://search-scalable-final-proj-knlaqfap2p2aavmspdlhxboypm.ap-southeast-1.es.amazonaws.com/'
+ES_PORT = '9200'
+ELASTIC_URI = f'{ES_HOST}:{ES_PORT}'
--- apis/products.py
+++ apis/products.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, jsonify, render_template, request, json
+from tomlkit import document
 from config import *
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+from accesskeys.aws_secrets import * #This is hidden from github, but you can use this to store your AWS credentials
 
 app = Flask(__name__)
+es = Elasticsearch(
+    hosts=ES_HOST,
+    http_auth=AWS4Auth(AWSAccessKeyId, AWSSecretKey, 'ap-southeast-1', 'es'), #For signing the request to Elasticsearch
+    verify_certs=True,
+    use_ssl=True,
+    connection_class=RequestsHttpConnection,
+    port=443
+)
+
+app.run(host='0.0.0.0', port=5000, debug=True)
 
 #PRODUCT LISTING CHARACTERISTICS
 """
@@ -13,27 +27,57 @@ price: float
 image_url: varchar ***maybe an array of varchars, for multiple pictures***
 """
 
+example_product = {
+    "product_id": 1,
+    "supplier_id": 1,
+    "description": "This is a test product",
+    "price": 1.00,
+    "image_url": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+}
+
+
+@app.route('/', methods['GET'])
+def index():
+    print('test call, if this don\'t print you fucked')
+    return jsonify(example_product)
+
 #create product
 @app.route(f'/{ELASTIC_PREFIX}/create', methods=['POST'])
 def publish():
     """
     Publish product to search engine
     """
-    data = request.get_json()
-    print(data)
-    return json.dumps({'status': 'success'})
+    content = request.json
+    document = content['product_document']
+    print(document)
+    resp = es.index(index='products', document = document, id=document['product_id'])
+    print(resp)
+    print("*"*30 + "PUBLISHED" + "*"*30)
+    print(resp['result'])
+    return jsonify(resp)
 
 #search for a product
 @app.route(f'/{ELASTIC_PREFIX}/search', methods=['POST'])
 def search():
-    ...
+    content = request.json
+    search_param = content['search_param']
+    resp = es.search(index='products', body={"query": {"match": {"description": search_param}}})
+    return jsonify(resp)
 
-
-@app.route(f'/{ELASTIC_PREFIX}/delete', methods=['DELETE'])
-def delete():
-    ...
 
 @app.route(f'/{ELASTIC_PREFIX}/update', methods=['POST'])
 def update():
-    ...
+
+    content = request.json
+    document = content['product_document']
+    resp = es.update(index='products', id=document['product_id'], body={"doc": document})
+    return jsonify(resp)    
+
+@app.route(f'/{ELASTIC_PREFIX}/delete', methods=['DELETE'])
+def delete():
+    content = request.json
+    product_id = content['product_id']
+    resp = es.delete(index='products', id=product_id)
+    return jsonify(resp)
+
     
