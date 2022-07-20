b7ddda90b1e520785c9186f9c69aa08ae4732f24
Nakarin Hansawattana <nakarinh14@gmail.com>
2022-07-20 19:13:19 +0700

    Add Gunicorn and Opentelemetry

--- apis/base.py
+++ apis/base.py
@@ -4,11 +4,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import *
+from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
+from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
 
 
+Psycopg2Instrumentor().instrument()
 
 
 Engine = create_engine(POSTGRES_DATABASE)
 Session = sessionmaker(bind=Engine)
 
 Base = declarative_base()
+
+
+SQLAlchemyInstrumentor().instrument(engine=Engine)
